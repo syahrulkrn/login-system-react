12bb2c9ef7834aef3eec6fa602bc06eca87d55e5
myusername <syahrulbelfort@gmail.com>
2024-05-17 20:32:44 +0700

    co: perubahan

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import PrivateRoutes from './components/PrivateRoutes';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<PrivateRoutes Component={Home} />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
+    // ini adalah perubahan
   );
 }
 
